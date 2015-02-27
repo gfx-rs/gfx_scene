3ea5f9a49761c44e1747d89f2c6620106b168107
Dzmitry Malyshau <kvarkus@gmail.com>
2015-02-26 23:42:12 -0500

    Implemented the Camera

--- src/scene/lib.rs
+++ src/scene/lib.rs
@@ -4,9 +4,6 @@ extern crate cgmath;
 
 use cgmath::{BaseFloat, Zero, Matrix3, Matrix4, Transform3};
 
-//TODO
-pub struct Camera<S>(S);
-
 #[derive(Debug)]
 pub enum DrawError {
     Batch(gfx::batch::BatchError),
@@ -14,28 +11,13 @@ pub enum DrawError {
 }
 
 pub trait AbstractScene<D: gfx::Device> {
-    type Scalar;
-    type Entity;
     type SpaceData;
+    type Entity;
+    type Camera;
 
     fn draw<P: draw::AbstractPhase<D, Self::SpaceData, Self::Entity> + ?Sized>(
-            &mut self, &mut P, &Camera<Self::Scalar>, &gfx::Frame<D::Resources>,
+            &mut self, &mut P, &Self::Camera, &gfx::Frame<D::Resources>,
             &mut gfx::Renderer<D::CommandBuffer>) -> Result<(), DrawError>;
-}
-
-pub struct Entity<R: gfx::Resources, M> {
-    material: M,
-    mesh: gfx::Mesh<R>,
-    slice: gfx::Slice<R>,
-}
-
-impl<R: gfx::Resources, M> draw::Entity<R, M> for Entity<R, M> {
-    fn get_material(&self) -> &M {
-        &self.material
-    }
-    fn get_mesh(&self) -> (&gfx::Mesh<R>, gfx::Slice<R>) {
-        (&self.mesh, self.slice)
-    }
 }
 
 /// A class that manages spatial relations between objects
@@ -50,16 +32,41 @@ pub trait World {
     fn iter_bones(&self, Self::SkeletonPtr) -> Self::Iter;
 }
 
-pub struct Scene<R: gfx::Resources, M, W> {
-    pub entities: Vec<Entity<R, M>>,
+pub struct Entity<R: gfx::Resources, M, W: World> {
+    pub name: String,
+    pub material: M,
+    mesh: gfx::Mesh<R>,
+    slice: gfx::Slice<R>,
+    node: W::NodePtr,
+    skeleton: Option<W::SkeletonPtr>,
+}
+
+impl<R: gfx::Resources, M, W: World> draw::Entity<R, M> for Entity<R, M, W> {
+    fn get_material(&self) -> &M {
+        &self.material
+    }
+    fn get_mesh(&self) -> (&gfx::Mesh<R>, gfx::Slice<R>) {
+        (&self.mesh, self.slice)
+    }
+}
+
+pub struct Camera<P, N> {
+    pub name: String,
+    pub projection: P,
+    pub node: N,
+}
+
+pub struct Scene<R: gfx::Resources, M, W: World, P> {
+    pub entities: Vec<Entity<R, M, W>>,
+    pub cameras: Vec<Camera<P, W::NodePtr>>,
     pub world: W,
     context: gfx::batch::Context<R>,
 }
 
 pub struct SpaceData<S> {
     depth: S,
-    _vertex_mx: Matrix4<S>,
-    _normal_mx: Matrix3<S>,
+    vertex_mx: Matrix4<S>,
+    normal_mx: Matrix3<S>,
 }
 
 impl<S: Copy + PartialOrd> draw::ToDepth for SpaceData<S> {
@@ -73,13 +80,14 @@ impl<
     D: gfx::Device,
     M: draw::Material,
     W: World,
-> AbstractScene<D> for Scene<D::Resources, M, W> {
-    type Scalar = W::Scalar;
-    type Entity = Entity<D::Resources, M>;
+    P: cgmath::Projection<W::Scalar>,
+> AbstractScene<D> for Scene<D::Resources, M, W, P> {
     type SpaceData = SpaceData<W::Scalar>;
+    type Entity = Entity<D::Resources, M, W>;
+    type Camera = Camera<P, W::NodePtr>;
 
-    fn draw<P: draw::AbstractPhase<D, SpaceData<W::Scalar>, Entity<D::Resources, M>> + ?Sized>(
-            &mut self, phase: &mut P, _camera: &Camera<W::Scalar>,
+    fn draw<H: draw::AbstractPhase<D, SpaceData<W::Scalar>, Entity<D::Resources, M, W>> + ?Sized>(
+            &mut self, phase: &mut H, _camera: &Camera<P, W::NodePtr>,
             frame: &gfx::Frame<D::Resources>,
             renderer: &mut gfx::Renderer<D::CommandBuffer>)
             -> Result<(), DrawError> {
@@ -91,8 +99,8 @@ impl<
             //TODO: compute depth here
             let data = SpaceData {
                 depth: Zero::zero(),
-                _vertex_mx: Matrix4::identity(),
-                _normal_mx: Matrix3::identity(),
+                vertex_mx: Matrix4::identity(),
+                normal_mx: Matrix3::identity(),
             };
             match phase.enqueue(entity, data, &mut self.context) {
                 Ok(()) => (),
@@ -115,10 +123,10 @@ pub struct PhaseHarness<D: gfx::Device, C, P> {
 impl<
     D: gfx::Device,
     C: AbstractScene<D>,
-    P: draw::AbstractPhase<D, C::SpaceData, C::Entity>
-> PhaseHarness<D, C, P> {
-    pub fn draw(&mut self, camera: &Camera<C::Scalar>,
-                frame: &gfx::Frame<D::Resources>) -> Result<(), DrawError> {
+    H: draw::AbstractPhase<D, C::SpaceData, C::Entity>
+> PhaseHarness<D, C, H> {
+    pub fn draw(&mut self, camera: &C::Camera, frame: &gfx::Frame<D::Resources>)
+                -> Result<(), DrawError> {
         self.renderer.reset();
         for phase in self.phases.iter_mut() {
             match self.scene.draw(phase, camera, frame, &mut self.renderer) {
@@ -130,12 +138,19 @@ impl<
     }
 }
 
-/// A typical scene to be used in demoes. Has a heterogeneous vector of phases.
+pub type PerspectiveCam<W: World> = Camera<
+    cgmath::PerspectiveFov<W::Scalar, cgmath::Rad<W::Scalar>>,
+    W::NodePtr
+>;
+
+/// A typical scene to be used in demoes. Has a heterogeneous vector of phases
+/// and a perspective fov-based camera.
 pub type StandardScene<
     D: gfx::Device,
     M: draw::Material,
     W: World,
-> = PhaseHarness<
-    D, Scene<D::Resources, M, W>,
-    Box<draw::AbstractPhase<D, SpaceData<W::Scalar>, Entity<D::Resources, M>>>
+    P: cgmath::Projection<W::Scalar>,
+> = PhaseHarness<D,
+    Scene<D::Resources, M, W, P>,
+    Box<draw::AbstractPhase<D, SpaceData<W::Scalar>, Entity<D::Resources, M, W>>>
 >;
