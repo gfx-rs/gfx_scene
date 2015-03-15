9dbf6eeeb548b98b1c502db7d7a48af39cfdcd5e
Dzmitry Malyshau <kvarkus@gmail.com>
2015-03-15 09:33:17 +0300

    Commented gfx_scene

--- src/scene/lib.rs
+++ src/scene/lib.rs
@@ -1,25 +1,40 @@
+#![deny(missing_docs)]
+
+//! Scene infrastructure to be used with Gfx phases.
+
 extern crate "gfx_phase" as phase;
 extern crate gfx;
 extern crate cgmath;
 
 use std::marker::PhantomData;
 
+/// Scene drawing error.
 #[derive(Debug)]
 pub enum Error {
+    /// Error in creating a batch.
     Batch(gfx::batch::Error),
+    /// Error in sending a batch for drawing.
     Flush(phase::FlushError),
 }
 
+/// Abstract information about the view. Supposed to containt at least
+/// Model-View-Projection transform for the shader.
 pub trait ViewInfo<S, T: cgmath::Transform3<S>>: phase::ToDepth<Depth = S> {
+    /// Construct a new information block.
     fn new(mvp: cgmath::Matrix4<S>, view: T, model: T) -> Self;
 }
 
 /// Abstract scene that can be drawn into something
 pub trait AbstractScene<D: gfx::Device> {
+    /// A type of the view information.
     type ViewInfo;
+    /// A type of the entity.
     type Entity;
+    /// A type of the camera.
     type Camera;
 
+    /// Draw the contents of the scene with a specific phase into a renderer,
+    /// using a given camera and a frame.
     fn draw<H: phase::AbstractPhase<D, Self::Entity, Self::ViewInfo> + ?Sized>(
             &mut self, &mut H, &Self::Camera, &gfx::Frame<D::Resources>,
             &mut gfx::Renderer<D::Resources, D::CommandBuffer>) -> Result<(), Error>;
@@ -27,24 +42,36 @@ pub trait AbstractScene<D: gfx::Device> {
 
 /// A class that manages spatial relations between objects
 pub trait World {
+    /// Type of the scalar used in all associated mathematical constructs.
     type Scalar: cgmath::BaseFloat + 'static;
+    /// Type of the rotation that can be decomposed from the transform.
     type Rotation: cgmath::Rotation3<Self::Scalar>;
+    /// Type of the transform that every node performs relative to the parent.
     type Transform: cgmath::CompositeTransform3<Self::Scalar, Self::Rotation> + Clone;
+    /// Pointer to a node, associated with an entity, camera, or something else.
     type NodePtr;
+    /// Pointer to a skeleton, associated with an enttity.
     type SkeletonPtr;
+    /// Iterator over transformations, used for walking the bones.
     type Iter: Iterator<Item = Self::Transform>;
-
+    /// Get the transfrormation of a specific node pointer.
     fn get_transform(&self, &Self::NodePtr) -> &Self::Transform;
+    /// Iterate over the bones of a specific skeleton.
     fn iter_bones(&self, &Self::SkeletonPtr) -> Self::Iter;
 }
 
+/// A simple struct representing an object with a given material, mesh, bound,
+/// and spatial relation to other stuff in the world.
 pub struct Entity<R: gfx::Resources, M, W: World, B> {
+    /// Name of the entity.
     pub name: String,
+    /// Assotiated material of the entity.
     pub material: M,
     mesh: gfx::Mesh<R>,
     slice: gfx::Slice<R>,
     node: W::NodePtr,
     skeleton: Option<W::SkeletonPtr>,
+    /// Associated spatial bound of the entity.
     pub bound: B,
 }
 
@@ -57,12 +84,19 @@ impl<R: gfx::Resources, M: phase::Material, W: World, B> phase::Entity<R, M> for
     }
 }
 
+/// A simple camera with generic projection and spatial relation.
 pub struct Camera<P, N> {
+    /// Name of the camera.
     pub name: String,
+    /// Generic projection.
     pub projection: P,
+    /// Generic spatial node.
     pub node: N,
 }
 
+/// A generic `draw()` routine that takes a phase and some entities, and draws them
+/// into a given frame. It does frustum culling and `ViewInfo` construction.
+/// It can be used as a helper for user-side scenes.
 pub fn draw_entities<'a,
     D: gfx::Device,
     M: phase::Material + 'a,
@@ -118,9 +152,13 @@ where
          .map_err(|e| Error::Flush(e))
 }
 
+/// An example scene type.
 pub struct Scene<R: gfx::Resources, M, W: World, B, P, V> {
+    /// A list of entities in the scene.
     pub entities: Vec<Entity<R, M, W, B>>,
+    /// A list of cameras.
     pub cameras: Vec<Camera<P, W::NodePtr>>,
+    /// Spatial world.
     pub world: W,
     context: gfx::batch::Context<R>,
     _view_dummy: PhantomData<V>,
@@ -151,8 +189,11 @@ impl<
 /// Wrapper around a scene that carries a list of phases as well as the
 /// `Renderer`, allowing to isolate a command buffer completely.
 pub struct PhaseHarness<D: gfx::Device, C: AbstractScene<D>> {
+    /// Wrapped scene.
     pub scene: C,
+    /// List of phases as trait objects.
     pub phases: Vec<Box<phase::AbstractPhase<D, C::Entity, C::ViewInfo>>>,
+    /// Gfx renderer to draw into.
     pub renderer: gfx::Renderer<D::Resources, D::CommandBuffer>,
 }
 
@@ -160,6 +201,7 @@ impl<
     D: gfx::Device,
     C: AbstractScene<D>,
 > PhaseHarness<D, C> {
+    /// Draw the scene into a given frame, using all the phases. 
     pub fn draw(&mut self, camera: &C::Camera, frame: &gfx::Frame<D::Resources>)
                 -> Result<(), Error> {
         use std::ops::DerefMut;
@@ -170,10 +212,11 @@ impl<
                 Err(e) => return Err(e),
             }
         }
-        Ok(())
+        Ok(()) //TODO: return a command buffer?
     }
 }
 
+/// A simple perspective camera based on the `World` trait.
 pub type PerspectiveCam<W: World> = Camera<
     cgmath::PerspectiveFov<W::Scalar, cgmath::Rad<W::Scalar>>,
     W::NodePtr
